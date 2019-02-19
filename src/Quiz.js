42038228ea2987705ef0606bb4144182ace08921
Haoyu ZUO <zhyphil@gmail.com>
2019-02-19 15:14:33 +0100

    Displaying multiple QuizQuestionButtons

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,12 +10,17 @@ class Quiz extends Component {
     this.state = {quiz_position: 1}
   }
 
+  showNextQuestion(){
+      this.setState((state)=>{
+        return {quiz_position: state.quiz_position + 1}
+      });
+  }
   render(){
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
     return (
       <div>
       {isQuizEnd ? <QuizEnd />:
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
       </div>
     )
   }
